6559a4318cc75be976a2a7159d0d114e426293ee
ljxk <102322415+ljxk@users.noreply.github.com>
2022-04-06 19:25:48 +0000

    eek!

---
+++ clothes.py
@@ -0,0 +1,12 @@
+class shirt:
+  def __init__ (self,_color,_numbuttons,_ishortsleeve,_hascollar):
+    self.color = _color
+    self.numbuttons = _numbuttons
+    self.ishortsleeve = _ishortsleeve
+    self.hascollar = _hascollar
+
+class pants:
+  def __init__ (self,_waistsize,_length,_color):
+    self.waistsize = _waistsize
+    self.length = _length
+    self.color = _color
